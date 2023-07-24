84dfdf1c460ee90c3979b1ad31a1951e65587434
SGDoubler <doublesn@mail.uc.edu>
2023-07-24 11:34:05 -0400

    Part 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int SIZE = 100;
+        int[] dataPoints = new int[SIZE];
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+        for (int x = 0; x < dataPoints.length; x++){
+            dataPoints[x] = rnd.nextInt(100) + 1;
+        }
+        for (int x = 0; x < dataPoints.length; x++){
+            System.out.printf("%5d" + " | ", dataPoints[x]);
+        }
+        int sum = 0;
+        for (int x = 0; x < dataPoints.length; x++){
+            sum = sum + dataPoints[x];
+        }
+        int avg = sum / dataPoints.length;
+        System.out.println("\nThe sum of all of the integers in the random array dataPoints is:" + sum);
+        System.out.println("The average of the random array dataPoints is:" + avg);
+
+        // Part 2 Linear Scan
+
+        int
     }
 }
